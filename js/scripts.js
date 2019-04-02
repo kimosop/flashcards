4df483766f073d2a0d79e2b6bdbc8395f0f1149b
Victor Kimosop Chepkoit <VCHEPKOIT@safaricom.co.ke>
2019-04-02 18:43:19 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#js").click(function() {
+    $(".javascript-showing").toggle();
+    $(".javascript-hidden").toggle();
+  });
+  $("#ops").click(function() {
+    $(".operators-showing").toggle();
+    $(".operators-hidden").toggle();
+  });
+
+});
